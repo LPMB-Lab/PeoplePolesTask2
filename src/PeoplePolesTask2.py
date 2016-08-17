611f4782730992e2a1c7d93278fc88ed3faa5e56
Omas Abdullah <omas.abdullah@gmail.com>
2016-08-17 16:28:25 -0400

    Loads of overhaul

--- src/PeoplePolesTask2.py
+++ src/PeoplePolesTask2.py
@@ -11,6 +11,7 @@ import math
 import time
 import array
 import datetime
+import steamvr
 
 ###################
 ##  CONSTANTS
@@ -24,130 +25,147 @@ KEYS = { 'reset'	: 'r'
 }
 
 OPTOTRAK_IP = '192.219.236.36'
+HMD = HMD_TYPE['VIVE']
+TRACKING = TRACKING_TYPE['VIVE']
 
-NO_GVS 		= "NGVS"
-LEFT_GVS 	= "LGVS"
-RIGHT_GVS 	= "RGVS"
+# Real Trials
+CONDITIONS = ['POLES', 'AVATAR_FORWARD', 'AVATAR_BACKWARD']
+APERTURES = [1.0, 1.4, 1.8]
+TRIALS_PER_APERTURE = 5
+
+# Catch Trials
+C_CONDITION = ['AVATAR_FORWARD_MOVE', 'AVATAR_BACKWARD_MOVE']
+C_APERTURES = [1.0, 1.4, 1.8]
+CTRIALS_PER_APERTURE = 1
+
+HMD_TYPE = {
+	'OCULUS': 0,
+	'VIVE': 1,
+	'NONE': 2
+}
+
+TRACKING_TYPE = { 
+	'OPTOTRAK': 0,
+	'VIVE': 1,
+	'NONE': 2
+}
 
 # Locations for cylinders during learning phase
-learnCylinderLocations = [
+learncylinderlocations = [
 	[-1,0,-1.5],
 	[-1,0,1.5],
 	[1,0,1.5],
 	[1,0,-1.5]]
-	
-# 4 Triangles for testing
-TWO_THREE_LEFT_TRIANGLE = [
-	[-1,0,-1.5],
-	[1,0,-1.5],
-	[1,0,1.5],
-	["TWO_THREE_LEFT_TRIANGLE"]
-]
-
-TWO_THREE_RIGHT_TRIANGLE = [
-	[1,0,-1.5],
-	[-1,0,-1.5],
-	[-1,0,1.5],
-	["TWO_THREE_RIGHT_TRIANGLE"]
-]
-
-THREE_TWO_LEFT_TRIANGLE = [
-	[-1,0,-1.5],
-	[-1,0,1.5],
-	[1,0,1.5],
-	["THREE_TWO_LEFT_TRIANGLE"]
-]
-
-THREE_TWO_RIGHT_TRIANGLE = [
-	[1,0,-1.5],
-	[1,0,1.5],
-	[-1,0,1.5],
-	["THREE_TWO_RIGHT_TRIANGLE"]
-]
 
 # The actual trials will be stored here
 trials = []
-	
-viz.setMultiSample(8)
-viz.fov(60)
-viz.go()
-
-################
-##  OCULUS
-################
-
-# Setup Oculus Rift HMD
-hmd = oculus.Rift()
-if not hmd.getSensor():
-	sys.exit('Oculus Rift not detected')
-
-# Go fullscreen if HMD is in desktop display mode
-if hmd.getSensor().getDisplayMode() == oculus.DISPLAY_DESKTOP:
-	viz.window.setFullscreen(True)
-
-# Setup heading reset key
-vizact.onkeydown(KEYS['reset'], hmd.getSensor().reset)
-
-# Link HMD Orientation to mainview
-viewLink = viz.link(hmd.getSensor(), viz.MainView, mask=viz.LINK_ORI)
-
-# Apply user profile eye height to view
-
-profile = hmd.getProfile()
-if profile:
-	viewLink.setOffset([0,profile.eyeHeight,0])
-else:
-	viewLink.setOffset([0,1.8,0])
-
-# Hide and trap mouse since we will be using virtual canvas mouse
-viz.mouse.setVisible(False)
-viz.mouse.setTrap(True)
-
-#####################
-## OPTOTRAK3
-#####################
-
-# This part is to be uncommented for the Optotrack stuff
-# Linking the avatar to the Optotrack rigid bodies
-opto = viz.add('optotrak.dle', 0, OPTOTRAK_IP)
-body = opto.getBody(0)
-optoLink = viz.link(body, viz.MainView, mask=viz.LINK_POS)
-	
-#####################
-## SIMULATION
-#####################
-
-#Set up the environment and proximity sensors
-scene = viz.addChild('newMaze.osgb')
-
-#Create proximity manager and set debug on. Toggle debug with d key
-manager = vizproximity.Manager()
-manager.setDebug(viz.ON)
-debugEventHandle = vizact.onkeydown(KEYS['debug'],manager.setDebug,viz.TOGGLE)
-
-#Add main viewpoint as proximity target
-target = vizproximity.Target(viz.MainView)
-manager.addTarget(target)
-
-#fade to true color when viewpoint moves near
-def EnterCylinder(e, cylinder):
-	cylinder.remove()
-
 #add cylinders and create a proximity sensor around each one
 cylinderSensors = []
+hmd = None
+viztask.schedule(experiment)
 
-#Add a sensor in the center of the room for the participant to return to after each trial
-centerSensor = vizproximity.Sensor(vizproximity.CircleArea(1.5,center=(0.0,0.0)),None)
-manager.addSensor(centerSensor)
+def initializeHMD():
+	if HMD == HMD_TYPE['OCULUS']:
+		# Setup Oculus Rift HMD
+		hmd = oculus.Rift()
+		if not hmd.getSensor():
+			sys.exit('Oculus Rift not detected')
 
-#Add vizinfo panel to display instructions
-info = vizinfo.InfoPanel("Explore the environment")
+		# Go fullscreen if HMD is in desktop display mode
+		if hmd.getSensor().getDisplayMode() == oculus.DISPLAY_DESKTOP:
+			viz.window.setFullscreen(True)
+
+		# Setup heading reset key
+		vizact.onkeydown(KEYS['reset'], hmd.getSensor().reset)
+
+		# Link HMD Orientation to mainview
+		viewLink = viz.link(hmd.getSensor(), viz.MainView, mask=viz.LINK_ORI)
+
+		# Apply user profile eye height to view
+
+		profile = hmd.getProfile()
+		if profile:
+			viewLink.setOffset([0,profile.eyeHeight,0])
+		else:
+			viewLink.setOffset([0,1.8,0])
+			
+		hideMouse()
+	elif HMD == HMD_TYPE['VIVE']:
+		hmd = steamvr.HMD()
+
+		if not hmd.getSensor():
+			sys.exit('Vive not detected')
+		viz.link(hmd.getSensor(), viz.MainView)
+		
+		hideMouse()
+	else:
+		print("No HMD Set")
+
+def initializeTracking():
+	if TRACKING == TRACKING_TYPE['OPTOTRAK']:
+		# This part is to be uncommented for the Optotrack stuff
+		# Linking the avatar to the Optotrack rigid bodies
+		opto = viz.add('optotrak.dle', 0, OPTOTRAK_IP)
+		body = opto.getBody(0)
+		optoLink = viz.link(body, viz.MainView, mask=viz.LINK_POS)
+
+def hideMouse():
+	# Hide and trap mouse since we will be using virtual canvas mouse
+	viz.mouse.setVisible(False)
+	viz.mouse.setTrap(True)
+
+def initializeSimulation():
+	# Preliminary setup
+	viz.setMultiSample(8)
+	viz.fov(60)
+	viz.go()
+
+	#Set up the environment and proximity sensors
+	scene = viz.addChild('../assets/newMaze.osgb')
+
+	#Create proximity manager and set debug on. Toggle debug with d key
+	manager = vizproximity.Manager()
+	manager.setDebug(viz.ON)
+	debugEventHandle = vizact.onkeydown(KEYS['debug'],manager.setDebug,viz.TOGGLE)
+
+	#Add main viewpoint as proximity target
+	target = vizproximity.Target(viz.MainView)
+	manager.addTarget(target)	
+
+	#Add a sensor in the center of the room for the participant to return to after each trial
+	centerSensor = vizproximity.Sensor(vizproximity.CircleArea(1.5,center=(0.0,0.0)),None)
+	manager.addSensor(centerSensor)
+
+	#Add vizinfo panel to display instructions
+	info = vizinfo.InfoPanel("Explore the environment")
+
+def experiment():
+	initializeHMD()
+	initializeTracking()
+	initializeSimulation()
+	GenerateTrials()
+	ExportTrials()
+	
+	#Wait for spacebar to begin experiment
+	yield viztask.waitKeyDown(KEYS['start'])
+	yield learnPhase()
+	yield testPhase()
+
+	#Log results to file
+	try:
+		with open('experiment_data.txt','w') as f:
+
+			#write trial data to file
+			for i in range(0, len(trials)):
+				data += "{0}. {1}".format(i, trials[i][1])
+			f.write(data)
+	except IOError:
+		viz.logWarn('Could not log results to file. Make sure you have permission to write to folder')
 
 def ExportTrials():
 	i = datetime.datetime.now()
 	fileName = "%s_%s_%s_%s_%s_%s.txt" % (i.year, i.month, i.day, i.hour, i.minute, i.second)
 	print fileName
-	
 	
 	export_data = open(str(fileName), 'a')
 	
@@ -155,9 +173,8 @@ def ExportTrials():
 		export_data.write( str(i+1) + ". " + str(trials[i][1]) + ", " + str(trials[i][0][3][0]) + "\n")
 		
 	export_data.flush()
-		
-def AddCylinder(color, position):
 
+def AddCylinder(color, position):
 	cylinder = vizshape.addCylinder(height=5,radius=0.2)
 	cylinder.setPosition(position)
 	cylinder.color(color)
@@ -168,7 +185,11 @@ def AddCylinder(color, position):
 	
 	manager.addSensor(sensor)
 	manager.onEnter(sensor, EnterCylinder, cylinder)
-	
+
+#fade to true color when viewpoint moves near
+def EnterCylinder(e, cylinder):
+	cylinder.remove()
+		
 def GenerateTrials():
 	
 	# Left Start Trials
@@ -179,57 +200,12 @@ def GenerateTrials():
 
 	# Trial generation
 	for i in range (0, 3):
-		
 		# Left Triangle Right GVS
 		trial = [THREE_TWO_LEFT_TRIANGLE, RIGHT_GVS]
 		leftStartTrials.append(trial)
-		
-		# Left Triangle Left GVS
-		trial = [THREE_TWO_LEFT_TRIANGLE, LEFT_GVS]
-		leftStartTrials.append(trial)
-		
-		# Left Triangle No GVS
-		trial = [THREE_TWO_LEFT_TRIANGLE, NO_GVS]
-		leftStartTrials.append(trial)
-		leftStartTrials.append(trial)
-		
-		# Left Triangle2 Right GVS
-		trial = [TWO_THREE_LEFT_TRIANGLE, RIGHT_GVS]
-		leftStartTrials.append(trial)
-		
-		# Left Triangle2 Left GVS
-		trial = [TWO_THREE_LEFT_TRIANGLE, LEFT_GVS]
-		leftStartTrials.append(trial)
-		
-		# Left Triangle2 No GVS
-		trial = [TWO_THREE_LEFT_TRIANGLE, NO_GVS]
-		leftStartTrials.append(trial)
-		leftStartTrials.append(trial)
-		
+
 		# Right Triangle Right GVS
 		trial = [THREE_TWO_RIGHT_TRIANGLE, RIGHT_GVS]
-		rightStartTrials.append(trial)
-		
-		# Right Triangle Left GVS
-		trial = [THREE_TWO_RIGHT_TRIANGLE, LEFT_GVS]
-		rightStartTrials.append(trial)
-		
-		# Right Triangle No GVS
-		trial = [THREE_TWO_RIGHT_TRIANGLE, NO_GVS]
-		rightStartTrials.append(trial)
-		rightStartTrials.append(trial)
-		
-		# Right Triangle2 Right GVS
-		trial = [TWO_THREE_RIGHT_TRIANGLE, RIGHT_GVS]
-		rightStartTrials.append(trial)
-		
-		# Right Triangle2 Left GVS
-		trial = [TWO_THREE_RIGHT_TRIANGLE, LEFT_GVS]
-		rightStartTrials.append(trial)
-		
-		# Right Triangle2 Right GVS
-		trial = [TWO_THREE_RIGHT_TRIANGLE, NO_GVS]
-		rightStartTrials.append(trial)
 		rightStartTrials.append(trial)
 
 	# Shuffle the Trials
@@ -243,8 +219,6 @@ def GenerateTrials():
 		trials.append(rightStartTrials[i])
 
 def learnPhase():
-
-	
 	# Provide instructions for the participant
 	info.setText("Walk to each pillar that appears.")
 
@@ -268,7 +242,6 @@ def learnPhase():
 	yield viztask.waitTime(5)
 
 def testPhase():
-	
 	for i in range (0, len(trials)):
 		
 		print("Test Number: " + str(i + 1))
@@ -295,28 +268,3 @@ def testPhase():
 
 
 	info.setText('Thank You. You have completed the experiment')
-
-def experiment():
-
-	# Generate the trials
-	GenerateTrials()
-	
-	ExportTrials()
-	
-	#Wait for spacebar to begin experiment
-	yield viztask.waitKeyDown(KEYS['start'])
-	yield learnPhase()
-	yield testPhase()
-
-	#Log results to file
-	try:
-		with open('experiment_data.txt','w') as f:
-
-			#write trial data to file
-			for i in range(0, len(trials)):
-				data += "{0}. {1}".format(i, trials[i][1])
-			f.write(data)
-	except IOError:
-		viz.logWarn('Could not log results to file. Make sure you have permission to write to folder')
-
-viztask.schedule(experiment)
