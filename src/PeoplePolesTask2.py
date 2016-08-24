8f01a397b96e37955b2b6418db73abe6a451834a
Omas Abdullah <omas.abdullah@gmail.com>
2016-08-24 00:48:57 -0400

    Fixed class structure and lowered ceiling

--- src/PeoplePolesTask2.py
+++ src/PeoplePolesTask2.py
@@ -21,7 +21,7 @@ import steamvr
 KEYS = { 'reset'	: 'r'
 		,'debug'	: 'd'
 		,'start'	: 's'
-		,'nextTrial': ' '
+		,'nextTrial': ' ' # Refers to a space bar
 }
 
 OPTOTRAK_IP = '192.219.236.36'
@@ -52,17 +52,15 @@ HMD = HMD_TYPE['NONE']
 TRACKING = TRACKING_TYPE['NONE']
 
 # Locations for cylinders during learning phase
-learncylinderlocations = [
+learnCylinderLocations = [
 	[-1,0,-1.5],
 	[-1,0,1.5],
 	[1,0,1.5],
 	[1,0,-1.5]]
 
-# The actual trials will be stored here
-trials = []
-#add cylinders and create a proximity sensor around each one
-cylinderSensors = []
-hmd = None
+###################
+##  TRIAL
+###################
 
 class Trial():
 	def __init__(self, condition, aperture):
@@ -71,88 +69,204 @@ class Trial():
 	
 	def toString(self):
 		return "%0.1f, %s" % (self.aperture, self.condition)
-	
-def initializeHMD():
-	if HMD == HMD_TYPE['OCULUS']:
-		# Setup Oculus Rift HMD
-		hmd = oculus.Rift()
-		if not hmd.getSensor():
-			sys.exit('Oculus Rift not detected')
 
-		# Go fullscreen if HMD is in desktop display mode
-		if hmd.getSensor().getDisplayMode() == oculus.DISPLAY_DESKTOP:
-			viz.window.setFullscreen(True)
+###################
+##  SIMULATION
+###################
 
-		# Setup heading reset key
-		vizact.onkeydown(KEYS['reset'], hmd.getSensor().reset)
-
-		# Link HMD Orientation to mainview
-		viewLink = viz.link(hmd.getSensor(), viz.MainView, mask=viz.LINK_ORI)
-
-		# Apply user profile eye height to view
-
-		profile = hmd.getProfile()
-		if profile:
-			viewLink.setOffset([0,profile.eyeHeight,0])
-		else:
-			viewLink.setOffset([0,1.8,0])
-			
-		hideMouse()
-	elif HMD == HMD_TYPE['VIVE']:
-		hmd = steamvr.HMD()
-
-		if not hmd.getSensor():
-			sys.exit('Vive not detected')
-		viz.link(hmd.getSensor(), viz.MainView)
+class PeoplePolesTask2():
+	def __init__(self):
+		self.trials = []
+		self.cylinderSensors = []
+		self.hmd = None
+		self.info = vizinfo.InfoPanel("")
+		self.manager = vizproximity.Manager()
+		self.learnCylinderLocations = [
+			[-1,0,-1.5],
+			[-1,0,1.5],
+			[1,0,1.5],
+			[1,0,-1.5]]
 		
-		hideMouse()
-	else:
-		print("No HMD Set")
+		self.initializeSimulation()
+	
+	def initializeSimulation(self):
+		
+		#Set up the environment and proximity sensors
+		scene = viz.addChild('../assets/newMaze.osgb')
 
-def initializeTracking():
-	if TRACKING == TRACKING_TYPE['OPTOTRAK']:
-		# This part is to be uncommented for the Optotrack stuff
-		# Linking the avatar to the Optotrack rigid bodies
-		opto = viz.add('optotrak.dle', 0, OPTOTRAK_IP)
-		body = opto.getBody(0)
-		optoLink = viz.link(body, viz.MainView, mask=viz.LINK_POS)
+		#Create proximity manager and set debug on. Toggle debug with d key
+		self.manager.setDebug(viz.ON)
+		debugEventHandle = vizact.onkeydown(KEYS['debug'], self.manager.setDebug,viz.TOGGLE)
 
-def hideMouse():
-	# Hide and trap mouse since we will be using virtual canvas mouse
-	viz.mouse.setVisible(False)
-	viz.mouse.setTrap(True)
+		#Add main viewpoint as proximity target
+		target = vizproximity.Target(viz.MainView)
+		self.manager.addTarget(target)
 
-def initializeSimulation():
-	#Set up the environment and proximity sensors
-	scene = viz.addChild('../assets/newMaze.osgb')
+		#Add a sensor in the center of the room for the participant to return to after each trial
+		centerSensor = vizproximity.Sensor(vizproximity.CircleArea(1.5,center=(0.0,0.0)),None)
+		self.manager.addSensor(centerSensor)
+		
+		self.info.setText("Explore the environment")
+		
+		###################
+		## HMD Initialization
+		###################
+		
+		if HMD == HMD_TYPE['OCULUS']:
+			# Setup Oculus Rift HMD
+			hmd = oculus.Rift()
+			if not hmd.getSensor():
+				sys.exit('Oculus Rift not detected')
 
-	#Create proximity manager and set debug on. Toggle debug with d key
-	manager = vizproximity.Manager()
-	manager.setDebug(viz.ON)
-	debugEventHandle = vizact.onkeydown(KEYS['debug'],manager.setDebug,viz.TOGGLE)
+			# Go fullscreen if HMD is in desktop display mode
+			if hmd.getSensor().getDisplayMode() == oculus.DISPLAY_DESKTOP:
+				viz.window.setFullscreen(True)
 
-	#Add main viewpoint as proximity target
-	target = vizproximity.Target(viz.MainView)
-	manager.addTarget(target)	
+			# Setup heading reset key
+			vizact.onkeydown(KEYS['reset'], hmd.getSensor().reset)
 
-	#Add a sensor in the center of the room for the participant to return to after each trial
-	centerSensor = vizproximity.Sensor(vizproximity.CircleArea(1.5,center=(0.0,0.0)),None)
-	manager.addSensor(centerSensor)
+			# Link HMD Orientation to mainview
+			viewLink = viz.link(hmd.getSensor(), viz.MainView, mask=viz.LINK_ORI)
 
-	#Add vizinfo panel to display instructions
-	info = vizinfo.InfoPanel("Explore the environment")
+			# Apply user profile eye height to view
+
+			profile = hmd.getProfile()
+			if profile:
+				viewLink.setOffset([0,profile.eyeHeight,0])
+			else:
+				viewLink.setOffset([0,1.8,0])
+				
+			hideMouse()
+		elif HMD == HMD_TYPE['VIVE']:
+			hmd = steamvr.HMD()
+
+			if not hmd.getSensor():
+				sys.exit('Vive not detected')
+			viz.link(hmd.getSensor(), viz.MainView)
+			
+			hideMouse()
+		else:
+			print("No HMD Set")
+
+		###################
+		## Tracking Initialization
+		###################
+		if TRACKING == TRACKING_TYPE['OPTOTRAK']:
+			opto = viz.add('optotrak.dle', 0, OPTOTRAK_IP)
+			body = opto.getBody(0)
+			optoLink = viz.link(body, viz.MainView, mask=viz.LINK_POS)
+
+		###################
+		## Generate Trials
+		###################
+
+		# Trial generation
+		for i in range (0, TRIALS_PER_APERTURE):
+			for j in range (0, len(CONDITIONS)):
+				for k in range (0, len(APERTURES)):
+					self.trials.append(Trial(CONDITIONS[j], APERTURES[k]))
+		
+		# Catch trial generation
+		for i in range (0, CTRIALS_PER_APERTURE):
+			for j in range (0, len(C_CONDITIONS)):
+				for k in range (0, len(C_APERTURES)):
+					self.trials.append(Trial(C_CONDITIONS[j], C_APERTURES[k]))
+
+		random.shuffle(self.trials)
+		
+		###################
+		## Export Trials
+		###################
+		i = datetime.datetime.now()
+		fileName = "%s_%s_%s_%s_%s_%s.txt" % (i.year, i.month, i.day, i.hour, i.minute, i.second)
+		print fileName
+		
+		export_data = open(str(fileName), 'a')
+		
+		for i in range (0, len(self.trials)):
+			export_data.write("%s. %s \n" % (str(i+1), self.trials[i].toString()))
+			
+		export_data.flush()
+
+	def AddCylinder(self, color, position):
+		cylinder = vizshape.addCylinder(height=5,radius=0.2)
+		cylinder.setPosition(position)
+		cylinder.color(color)
+		
+		sensor = vizproximity.addBoundingBoxSensor(cylinder,scale=(1,1,1))
+		
+		self.cylinderSensors.append(sensor)
+		
+		self.manager.addSensor(sensor)
+		self.manager.onEnter(sensor, self.EnterCylinder, cylinder)
+
+	#fade to true color when viewpoint moves near
+	def EnterCylinder(self, e, cylinder):
+		cylinder.remove()
+
+	def learnPhase(self):
+		# Provide instructions for the participant
+		self.info.setText("Walk to each marker that appears.")
+
+		# Hide instructions after 5 seconds
+		yield viztask.waitTime(5)
+
+		for i in range (0,len(self.learnCylinderLocations)):
+			
+			self.AddCylinder(viz.RED, self.learnCylinderLocations[i])
+
+			# Get sensor for this trial
+			sensor = self.cylinderSensors[i]
+
+			# The yielded command returns a viz.Data object with information
+			# about the proximity event such as the sensor, target involved
+			yield vizproximity.waitEnter(sensor)
+
+		self.info.setText("Calibration is complete.")
+		
+		# Start testing phase after 5 seconds
+		yield viztask.waitTime(5)
+
+	def testPhase(self):
+		for i in range (0, len(self.trials)):
+			
+			print("Test Number %s : %s" % (str(i + 1), self.trials[i].toString()))
+			
+			for j in range (0, 3):
+				
+				if (j==0):
+					AddCylinder(viz.RED, trials[i][0][j])
+					print("Created cylinder #" + str(j+1))
+				else:
+					AddCylinder(viz.GREEN, trials[i][0][j])
+					print("Created cylinder #" + str(j+1))
+				
+				sensor = cylinderSensors[j + 3*i + len(learnCylinderLocations)]
+				print("Waiting for sensor collision with sensor #" + str(j+1))
+				info.setText(trials[i][1])
+				yield vizproximity.waitEnter(sensor)
+				
+				if (j == 2):
+					# Make screen black until nextTrial key is pressed
+					viz.scene(2)
+					yield viztask.waitKeyDown(KEYS['nextTrial'])
+					viz.scene(1)
+
+
+		info.setText('Thank You. You have completed the experiment')
+		
+	def hideMouse():
+		# Hide and trap mouse since we will be using virtual canvas mouse
+		viz.mouse.setVisible(False)
+		viz.mouse.setTrap(True)
 
 def experiment():
-	initializeHMD()
-	initializeTracking()
-	initializeSimulation()
-	GenerateTrials()
-	ExportTrials()
-	
+	simulation = PeoplePolesTask2()
+
 	#Wait for spacebar to begin experiment
 	yield viztask.waitKeyDown(KEYS['start'])
-	yield learnPhase()
-	yield testPhase()
+	yield simulation.learnPhase()
+	yield simulation.testPhase()
 
 	#Log results to file
 	try:
@@ -164,102 +278,6 @@ def experiment():
 			f.write(data)
 	except IOError:
 		viz.logWarn('Could not log results to file. Make sure you have permission to write to folder')
-
-def ExportTrials():
-	i = datetime.datetime.now()
-	fileName = "%s_%s_%s_%s_%s_%s.txt" % (i.year, i.month, i.day, i.hour, i.minute, i.second)
-	print fileName
-	
-	export_data = open(str(fileName), 'a')
-	
-	for i in range (0, len(trials)):
-		#export_data.write( str(i+1) + ". " + trials[i].toString() + "\n")
-		export_data.write("%s. %s \n" % (str(i+1), trials[i].toString()))
-		
-	export_data.flush()
-
-def AddCylinder(color, position):
-	cylinder = vizshape.addCylinder(height=5,radius=0.2)
-	cylinder.setPosition(position)
-	cylinder.color(color)
-	
-	sensor = vizproximity.addBoundingBoxSensor(cylinder,scale=(1,1,1))
-	
-	cylinderSensors.append(sensor)
-	
-	manager.addSensor(sensor)
-	manager.onEnter(sensor, EnterCylinder, cylinder)
-
-#fade to true color when viewpoint moves near
-def EnterCylinder(e, cylinder):
-	cylinder.remove()
-		
-def GenerateTrials():
-	# Trial generation
-	for i in range (0, TRIALS_PER_APERTURE):
-		for j in range (0, len(CONDITIONS)):
-			for k in range (0, len(APERTURES)):
-				trials.append(Trial(CONDITIONS[j], APERTURES[k]))
-	
-	# Catch trial generation
-	for i in range (0, CTRIALS_PER_APERTURE):
-		for j in range (0, len(C_CONDITIONS)):
-			for k in range (0, len(C_APERTURES)):
-				trials.append(Trial(C_CONDITIONS[j], C_APERTURES[k]))
-
-	# Shuffle the Trials
-	random.shuffle(trials)
-
-def learnPhase():
-	# Provide instructions for the participant
-	info.setText("Walk to each marker that appears.")
-
-	# Hide instructions after 5 seconds
-	yield viztask.waitTime(5)
-
-	for i in range (0,len(learnCylinderLocations)):
-		
-		AddCylinder(viz.RED, learnCylinderLocations[i])
-
-		# Get sensor for this trial
-		sensor = cylinderSensors[i]
-
-		# The yielded command returns a viz.Data object with information
-		# about the proximity event such as the sensor, target involved
-		yield vizproximity.waitEnter(sensor)
-
-	info.setText("Calibration is complete.")
-	
-	# Start testing phase after 5 seconds
-	yield viztask.waitTime(5)
-
-def testPhase():
-	for i in range (0, len(trials)):
-		
-		print("Test Number: " + str(i + 1))
-		
-		for j in range (0, 3):
-			
-			if (j==0):
-				AddCylinder(viz.RED, trials[i][0][j])
-				print("Created cylinder #" + str(j+1))
-			else:
-				AddCylinder(viz.GREEN, trials[i][0][j])
-				print("Created cylinder #" + str(j+1))
-			
-			sensor = cylinderSensors[j + 3*i + len(learnCylinderLocations)]
-			print("Waiting for sensor collision with sensor #" + str(j+1))
-			info.setText(trials[i][1])
-			yield vizproximity.waitEnter(sensor)
-			
-			if (j == 2):
-				# Make screen black until nextTrial key is pressed
-				viz.scene(2)
-				yield viztask.waitKeyDown(KEYS['nextTrial'])
-				viz.scene(1)
-
-
-	info.setText('Thank You. You have completed the experiment')
 
 # Preliminary setup
 viz.setMultiSample(8)
